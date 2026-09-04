83df913a5303f365cb1b387c7e385e526a89c73e
Sujal Saroj <sujalsaroj2006@example.com>
2026-09-04 13:24:27 +0530

    Add APi frontend setup

--- API-monitoring/src/main/java/com/sujal/API_monitoring/dto/auth/LoginRequest.java
+++ API-monitoring/src/main/java/com/sujal/API_monitoring/dto/auth/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.sujal.API_monitoring.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -8,12 +9,21 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "Request body for User Login")
 public class LoginRequest {
 
-    
+    @Schema(
+            description = "User Email addrees",
+                    example = "sujal@gmail.com"
+    )
     @NotBlank(message = "Email is required" )
     @Email(message = "Email should be valid")
     private String email;
+
+    @Schema(
+            description = "Password for an account",
+                    example = "sujal@123"
+    )
     @NotBlank(message = "Password is required" )
     @Size(min=8, max=20 , message = "Password should be between 8 to 20 Character")
     private String password;
